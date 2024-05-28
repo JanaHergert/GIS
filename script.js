76d60ccf50ad6b1e6a1bcae6922acd8fce6b94c2
JanaHergert <jana_hergert@web.de>
2024-05-29 01:52:33 +0200

    update packinglist save list items

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
-document.getElementById("add").addEventListener("click", addElement);
+window.addEventListener("load", addHeaderFooter);
+function addHeaderFooter() {
+    // header + footer
+    let h = document.createElement("div");
+    let f = document.createElement("div");
+    h.innerHTML = `
+<h1>Meine Packliste</h1>
+`;
+    document.querySelector("header").appendChild(h);
+    f.innerHTML = `
+<h1>Gute Reise!</h1>
+        <i class="fas fa-solid fa-plane fa-2xl" style="color: white;"></i>
+        `;
+    document.querySelector("footer").appendChild(f);
+};
+
+
+let ul = document.getElementById("myUL");
+
+
+
+
+// add = ok button in class center
+if(document.getElementById("add")){
+    document.getElementById("add").addEventListener("click", addElement);
+
+}
 
 let lists = [];
 let currentListIndex = 0;
 
+//add element to center list
 function addElement() {
     let input = document.getElementById("myInput").value;
     if (input === '') {
         alert("Bitte gib etwas ein!");
         return;
     }
-
-    let ul = document.getElementById("myUL");
+    // print needs myUL
     let li = document.createElement("li");
 
+    // checkbox for list item + checked state
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.addEventListener("change", function() {
+    checkbox.addEventListener("change", function () {
         if (this.checked) {
             li.style.textDecoration = "line-through";
         } else {
@@ -24,14 +51,16 @@ function addElement() {
     });
     li.appendChild(checkbox);
 
+    // list item text
     let text = document.createTextNode(input);
     li.appendChild(text);
 
+    // list item trash icon + delete function
     let trashIcon = document.createElement("i");
     trashIcon.className = "fas fa-solid fa-trash fa-xs";
     trashIcon.style.color = "#008b8b";
     trashIcon.style.cursor = "pointer";
-    trashIcon.addEventListener("click", function() {
+    trashIcon.addEventListener("click", function () {
         ul.removeChild(li);
         lists[currentListIndex].items = lists[currentListIndex].items.filter(item => item !== input);
     });
@@ -43,9 +72,20 @@ function addElement() {
     lists[currentListIndex].items.push(input);
 }
 
-document.getElementById("newListButton").addEventListener("click", addNewList);
-document.getElementById("myLists").addEventListener("click", showLists);
 
+
+
+
+
+// newListButton = Neue Liste button in class right
+if(document.getElementById("newListButton")){
+document.getElementById("newListButton").addEventListener("click", addNewList);
+}
+if(document.getElementById("myLists")){
+document.getElementById("myLists").addEventListener("click", showLists);
+}
+
+// alert for right list title
 function addNewList() {
     let title = prompt("Bitte gib den Titel der neuen Liste ein:");
     if (title === null || title.trim() === "") {
@@ -64,13 +104,14 @@ function addNewList() {
     currentListIndex = lists.length - 1;
 
     let newRightColumn = document.createElement("div");
-newRightColumn.className = "right";
-newRightColumn.id = "rList";
+    newRightColumn.className = "right";
+    newRightColumn.id = "rList";
 
-newRightColumn.innerHTML = `
+    newRightColumn.innerHTML = `
     <span>
         <h2 contenteditable="true" onblur="updateTitle(this)">${title}</h2> 
         <i class="fas fa-solid fa-print" onclick="showPrintOptions()"></i>
+        <i class="fa-regular fa-floppy-disk" style="color: #008b8b;"></i>
         <i class="fas fa-solid fa-xmark" onclick="resetRightColumn();"></i>
     </span>
     <div class="input-container">
@@ -82,6 +123,7 @@ newRightColumn.innerHTML = `
     document.querySelector(".column").appendChild(newRightColumn);
 }
 
+// fill right list
 function addNewElement(button) {
     let input = button.previousElementSibling.value;
     if (input === '') {
@@ -94,7 +136,7 @@ function addNewElement(button) {
 
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.addEventListener("change", function() {
+    checkbox.addEventListener("change", function () {
         if (this.checked) {
             li.style.textDecoration = "line-through";
         } else {
@@ -110,7 +152,7 @@ function addNewElement(button) {
     trashIcon.className = "fas fa-solid fa-trash fa-xs";
     trashIcon.style.color = "#008b8b";
     trashIcon.style.cursor = "pointer";
-    trashIcon.addEventListener("click", function() {
+    trashIcon.addEventListener("click", function () {
         ul.removeChild(li);
     });
     li.appendChild(trashIcon);
@@ -118,6 +160,7 @@ function addNewElement(button) {
     ul.appendChild(li);
     button.previousElementSibling.value = "";
 }
+
 
 function showLists() {
     let leftColumn = document.querySelector(".left");
@@ -128,7 +171,7 @@ function showLists() {
         let listItem = document.createElement("div");
         listItem.className = "list-item";
         listItem.textContent = list.title;
-        listItem.addEventListener("click", function() {
+        listItem.addEventListener("click", function () {
             showList(index);
         });
         listContainer.appendChild(listItem);
@@ -153,16 +196,15 @@ function showList(index) {
         </div>
         <ul id="myUL"></ul>
     `;
-    
-    document.getElementById("add").addEventListener("click", addElement);
-    
-    let ul = document.getElementById("myUL");
+
+    // document.getElementById("add").addEventListener("click", addElement);
+
     list.items.forEach(item => {
         let li = document.createElement("li");
-    
+
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        checkbox.addEventListener("change", function() {
+        checkbox.addEventListener("change", function () {
             if (this.checked) {
                 li.style.textDecoration = "line-through";
             } else {
@@ -170,31 +212,33 @@ function showList(index) {
             }
         });
         li.appendChild(checkbox);
-    
+
         let text = document.createTextNode(item);
         li.appendChild(text);
-    
+
         let trashIcon = document.createElement("i");
         trashIcon.className = "fas fa-solid fa-trash fa-xs";
         trashIcon.style.color = "#008b8b";
         trashIcon.style.cursor = "pointer";
-        trashIcon.addEventListener("click", function() {
+        trashIcon.addEventListener("click", function () {
             ul.removeChild(li);
             list.items = list.items.filter(i => i !== item);
         });
         li.appendChild(trashIcon);
-    
+
         ul.appendChild(li);
     });
 }
 
+
+// change center title
 function updateTitle(element) {
     lists[currentListIndex].title = element.textContent;
 }
 
+// reset center list
 function clearList() {
-    
-    let ul = document.getElementById("myUL");
+
     ul.innerHTML = ""; // Leere die Liste
 
     let titleElement = document.querySelector('.center h2');
@@ -205,22 +249,22 @@ function clearList() {
 
 
 
-function resetCenterColumn() {
-    let centerColumn = document.querySelector('.center');
-    centerColumn.innerHTML = `
-        <span>
-            <h2 contenteditable="true" onblur="updateTitle(this)">Reise nach XY</h2> 
-            <i class="fas fa-solid fa-print" onclick="showPrintOptions()"></i>
-            <i class="fas fa-solid fa-xmark" onclick="clearList()"></i>
-        </span>
-        <div class="input-container">
-            <input type="text" id="myInput" placeholder="Das fehlt noch...">
-            <button id="add" class="add">Ok</button>
-        </div>
-        <ul id="myUL"></ul>
-    `;
-    document.getElementById("add").addEventListener("click", addElement);
-}
+// function resetCenterColumn() {
+//     let centerColumn = document.querySelector('.center');
+//     centerColumn.innerHTML = `
+//         <span>
+//             <h2 contenteditable="true" onblur="updateTitle(this)">Reise nach XY</h2> 
+//             <i class="fas fa-solid fa-print" onclick="showPrintOptions()"></i>
+//             <i class="fas fa-solid fa-xmark" onclick="clearList()"></i>
+//         </span>
+//         <div class="input-container">
+//             <input type="text" id="myInput" placeholder="Das fehlt noch...">
+//             <button id="add" class="add">Ok</button>
+//         </div>
+//         <ul id="myUL"></ul>
+//     `;
+//     // document.getElementById("add").addEventListener("click", addElement);
+// }
 
 function resetRightColumn() {
     let rightColumn = document.querySelector('.right');
@@ -231,6 +275,7 @@ function resetRightColumn() {
 
 
 
+// placeholder print window (nonfunctional)
 function showPrintOptions() {
     let popup = document.createElement("div");
     popup.className = "popup";
@@ -253,6 +298,8 @@ function closePopup() {
     }
 }
 
+
+// open browser print window and print center list
 function PrintElem(elemId) {
     var ul = document.getElementById(elemId);
     var parentDiv = ul.closest('.center ');
@@ -269,7 +316,7 @@ function PrintElem(elemId) {
     printWindow.document.write('<style>');
     printWindow.document.write('@media print { body * { visibility: hidden; }');
     printWindow.document.write('#' + elemId + ', h2' + ', #' + elemId + ' * { visibility: visible; }');
-    printWindow.document.write('#' + elemId +', h2' + ' { position: relative;text-align: left; list-style-type: none; width: 100%; font-size: large;} }');
+    printWindow.document.write('#' + elemId + ', h2' + ' {font-family:Arial,Helvetica, sans-serif ; relative;text-align: left; list-style-type: none; width: 100%; font-size: large;} }');
     printWindow.document.write('</style>');
     printWindow.document.write('</head><body>');
     printWindow.document.write('<h2>');
@@ -283,4 +330,37 @@ function PrintElem(elemId) {
     printWindow.focus();
     printWindow.print();
     printWindow.close();
-    }
+}
+
+
+
+
+
+function SaveElem(elemId) {
+    var ul = document.getElementById(elemId);
+    var listItems = ul.querySelectorAll('li');
+    var listData = [];
+
+    listItems.forEach(function(item) {
+        // Remove the checkbox input and trash icon from the text content
+        var input = item.querySelector('input[type="checkbox"]');
+        var trashIcon = item.querySelector('i');
+        
+        // Temporarily hide input and icon to get only the text content
+        if (input) input.style.display = 'none';
+        if (trashIcon) trashIcon.style.display = 'none';
+
+        var text = item.textContent.trim();
+
+        // Restore the display of input and icon
+        if (input) input.style.display = '';
+        if (trashIcon) trashIcon.style.display = '';
+
+        listData.push({ text: text });
+    });
+
+    var jsonString = JSON.stringify(listData);
+    localStorage.setItem("listData", jsonString);
+
+    alert("Data saved: " + jsonString);
+}
